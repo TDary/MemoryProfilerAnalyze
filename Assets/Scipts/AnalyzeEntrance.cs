ff310fba88cb4c1aee0ea5766685e3606145874c
陈德睿 <w_chenderui@kingsoft.com>
2023-08-22 19:51:03 +0800

    UploadProject

---
+++ Assets/Scipts/AnalyzeEntrance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using static MemoryAnalyze.PackedMemorySnapshot;
+
+namespace MemoryAnalyze
+{
+    public class AnalyzeEntrance:UnityEditor.Editor
+    {
+        private string SnapFilePath;
+        [MenuItem("Test/Memory")]
+        static void Init()
+        {
+            string snapFilePath = "E:\\MemoryFroGitlab\\MemoryClient\\MemoryCaptures\\332785-格纳库-1080.snap";
+            var args = new PackedMemorySnapshotArgs();
+            args.source = UnityEditor.Profiling.Memory.Experimental.PackedMemorySnapshot.Load(snapFilePath);
+            var heap = PackedMemorySnapshot.GetMemoryProfilerData(args);
+        }
+    }
+}
--- Assets/Scipts/MemoryData/PackedMemorySnapshot.cs
+++ Assets/Scipts/MemoryData/PackedMemorySnapshot.cs
@@ -3,41 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace MemoryAnalyze
 {
-    public class PackedMemorySnapshot
+    public partial class PackedMemorySnapshot
     {
-        /// <summary>
-        /// An array of 4096bytes aligned memory sections. These appear to me the actual managed memory sections.
-        /// non-aligned sections seem to be internal / MonoMemPool sections.
-        /// </summary>
-        /// <see cref="https://forum.unity.com/threads/wip-heap-explorer-memory-profiler-debugger-and-analyzer-for-unity.527949/page-3#post-3902371"/>
-        public PackedMemorySection[] alignedManagedHeapSections = new PackedMemorySection[0];
+        public PackedMemorySnapshotHeader header = new PackedMemorySnapshotHeader();
 
-        /// <summary>
-        /// An array of extracted managed objects from the managed heap memory.
-        /// </summary>
-        public PackedManagedObject[] managedObjects = new PackedManagedObject[0];
+        // Descriptions of all the C++ unity types the profiled player knows about.
+        public PackedNativeType[] nativeTypes = new PackedNativeType[0];
 
-        /// <summary>
-        /// An array of managed static fields.
-        /// </summary>
-        public PackedManagedStaticField[] managedStaticFields = new PackedManagedStaticField[0];
+        // All native C++ objects that were loaded at time of the snapshot.
+        public PackedNativeUnityEngineObject[] nativeObjects = new PackedNativeUnityEngineObject[0];
 
-        /// <summary>
-        /// Indices into the managedTypes array of static types.
-        /// </summary>
-        public int[] managedStaticTypes = new int[0];
+        // All GC handles in use in the memorysnapshot.
+        public PackedGCHandle[] gcHandles = new PackedGCHandle[0];
 
-        /// <summary>
-        /// Indices into the connections array.
-        /// </summary>
-        public int[] connectionsToMonoScripts = new int[0];
+        // The unmodified connections array of "from -> to" pairs that describe which things are keeping which other things alive.
+        // connections 0..gcHandles.Length-1 represent connections FROM gchandles
+        // connections gcHandles.Length..connections.Length-1 represent connections FROM native
+        public PackedConnection[] connections = new PackedConnection[0];
 
-        /// <summary>
-        /// CoreTypes is a helper class that contains indices to frequently used classes, such as MonoBehaviour.
-        /// </summary>
-        public PackedCoreTypes coreTypes = new PackedCoreTypes();
+        // Array of actual managed heap memory sections. These are sorted by address after snapshot initialization.
+        public PackedMemorySection[] managedHeapSections = new PackedMemorySection[0];
+
+        // Descriptions of all the managed types that were known to the virtual machine when the snapshot was taken.
+        public PackedManagedType[] managedTypes = new PackedManagedType[0];
+
+        // Information about the virtual machine running executing the managade code inside the player.
+        public PackedVirtualMachineInformation virtualMachineInformation = new PackedVirtualMachineInformation();
+        public static PackedMemorySnapshot GetMemoryProfilerData(PackedMemorySnapshotArgs args)
+        {
+            var source = args.source;
+
+            var value = new PackedMemorySnapshot();
+            try
+            {
+                VerifyMemoryProfilerSnapshot(source);
+                value.header = PackedMemorySnapshotHeader.FromMemoryProfiler();
+                value.nativeTypes = PackedNativeType.FromMemoryProfiler(source);
+                value.nativeObjects = PackedNativeUnityEngineObject.FromMemoryProfiler(source);
+                value.gcHandles = PackedGCHandle.FromMemoryProfiler(source);
+                value.connections = PackedConnection.FromMemoryProfiler(source);
+                value.managedHeapSections = PackedMemorySection.FromMemoryProfiler(source);
+                value.managedTypes = PackedManagedType.FromMemoryProfiler(source);
+                value.virtualMachineInformation = PackedVirtualMachineInformation.FromMemoryProfiler(source);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogException(e);
+                value = null;
+                throw;
+            }
+            return value;
+        }
+        static void VerifyMemoryProfilerSnapshot(UnityEditor.Profiling.Memory.Experimental.PackedMemorySnapshot snapshot)
+        {
+            if (snapshot == null)
+                throw new Exception("No snapshot was found.");
+
+            if (snapshot.typeDescriptions == null || snapshot.typeDescriptions.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'typeDescriptions'. This is a known issue when using .NET 4.x Scripting Runtime.\n(Case 1079363) PackedMemorySnapshot: .NET 4.x Scripting Runtime breaks memory snapshot");
+
+            if (snapshot.managedHeapSections == null || snapshot.managedHeapSections.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'managedHeapSections'. This is a known issue when using .NET 4.x Scripting Runtime.\n(Case 1079363) PackedMemorySnapshot: .NET 4.x Scripting Runtime breaks memory snapshot");
+
+            if (snapshot.gcHandles == null || snapshot.gcHandles.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'gcHandles'. This is a known issue when using .NET 4.x Scripting Runtime.\n(Case 1079363) PackedMemorySnapshot: .NET 4.x Scripting Runtime breaks memory snapshot");
+
+            if (snapshot.nativeTypes == null || snapshot.nativeTypes.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'nativeTypes'.");
+
+            if (snapshot.nativeObjects == null || snapshot.nativeObjects.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'nativeObjects'.");
+
+            if (snapshot.connections == null || snapshot.connections.GetNumEntries() == 0)
+                throw new Exception("The snapshot does not contain any 'connections'.");
+        }
+        public class PackedMemorySnapshotArgs
+        {
+            public UnityEditor.Profiling.Memory.Experimental.PackedMemorySnapshot source;
+        }
     }
 }
